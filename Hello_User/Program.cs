6194319e49e877c4b4fe9b5d8433cdbc9297de68
Livhuwani <Livhuwani@LAPTOP-QTRB2ACR>
2022-08-14 18:15:31 +0200

    Add project files.

---
+++ Hello_User/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello_User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(":) Please enter your First Name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName + " " + "Welcome to C# practice with Kelebogile");
+            Console.ReadLine();
+        }
+    }
+}
